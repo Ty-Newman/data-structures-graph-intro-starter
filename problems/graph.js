f53730da6dc21be58bf71a1b6df10a7218c8755c
bryan <bryan.guner@gmail.com>
2020-10-28 16:43:46 -0400

    BFT in progress

--- problems/graph.js
+++ problems/graph.js
@@ -1,48 +1,51 @@
-
 class Graph {
-  constructor() {
-    this.adjList = {};
-  }
-
-  addVertex(vertex) {
-    if(!this.adjList[vertex]){
-      this.adjList[vertex] = [];
+    constructor() {
+        this.adjList = {};
     }
-  }
-
-  addEdges(srcValue, destValue) {
-    if(!this.adjList[srcValue]){
-      this.adjList[srcValue] = [];
+    addVertex(vertex) {
+        if (!this.adjList[vertex]) {
+            this.adjList[vertex] = [];
+        }
     }
-    if(!this.adjList[destValue]){
-      this.adjList[destValue] = [];
+    addEdges(srcValue, destValue) {
+        if (!this.adjList[srcValue]) {
+            this.adjList[srcValue] = [];
+        }
+        if (!this.adjList[destValue]) {
+            this.adjList[destValue] = [];
+        }
+        this.adjList[srcValue].push(destValue);
+        this.adjList[destValue].push(srcValue);
     }
-    this.adjList[srcValue].push(destValue);
-    this.adjList[destValue].push(srcValue);
-  }
-
-  buildGraph(edges) {
-    // Code goes here ...
-  }
-
-  breadthFirstTraversal(startingVertex) {
-    // Code goes here ...
-  }
-
-  depthFirstTraversalIterative(startingVertex) {
-    // Code goes here ...
-  }
-
-  depthFirstTraversalRecursive(startingVertex, visited = new Set(), vertices = []) {
-    // Code goes here ...
-  }
-
+    buildGraph(edges) {
+        let graph = {};
+        edges.forEach((connection) => {
+            this.addEdges(connection[0], connection[1]);
+        });
+        return this.adjList;
+    }
+    breadthFirstTraversal(startingVertex) {
+        //no recursion!
+        if (!startingVertex) return;
+        const visited = new Set();
+        const queue = [];
+        queue.push(startVertex);
+        visited.add();
+    }
+    depthFirstTraversalIterative(startingVertex) {
+        // Code goes here ...
+    }
+    depthFirstTraversalRecursive(
+        startingVertex,
+        visited = new Set(),
+        vertices = []
+    ) {
+        // Code goes here ...
+    }
 }
-
 let test = new Graph();
 // test.addVertex('a')
-console.log("test.addVertex('a')", test.addVertex('a'), test);
-
+console.log("test.addVertex('a')", test.addVertex("a"), test);
 module.exports = {
-  Graph
+    Graph,
 };
